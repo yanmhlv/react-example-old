3e1ebf35833f1d0ceb9c3ec60f085e569655d175
Yan Mihailov <yanmhlv@gmail.com>
2017-10-24 23:32:55 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
-import {React, Component} from 'react';
+import {React} from 'react';
 import ReactDOM from 'react-dom';
-// // import { Router, Route, browserHistory } from 'react-router';
-// // import {Welcome} from 'component/welcome';
-//
-// // import 'bootstrap/dist/css/bootstrap.min.css';
-// // import './assets/styles.css';
-//
-// class Welcome extends Component {
-//     // render() {
-//     //     return <h1>hello world</h1>;
-//     // }
-// }
-//
-// ReactDOM.render(
-//     <Welcome/>,
-//     document.getElementById('container')
-// );
 
+import App from './components/App.jsx';
 
-alert('hello world');
+ReactDOM.render(<App/>, document.getElementById('root'));

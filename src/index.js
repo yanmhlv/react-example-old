60b4c3bbc0ccb0bb39e6f5ce518ede939151fcd1
Yan Mihailov <yanmhlv@gmail.com>
2017-10-24 23:41:35 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import {React} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App.jsx';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.querySelector('#root'));
